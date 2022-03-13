97ff94c344230b21e3420a6d69ea1e1d686670cc
Xi Chu <xi.chu@tamu.edu>
2022-03-13 03:26:08 +0000

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root "floor#index"
-  get "/floor", to: "floor#index"
+  root to: redirect('/frontend/Untitled-1.html')
+  get "/login", :to => redirect('/frontend/Untitled-1.html')
+  #get "/floor", to: "floor#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
