470b83611d2f1773ab3e23f4191b9d4295b4d60f
Xi Chu <xi.chu@tamu.edu>
2022-03-13 04:12:15 +0000

    changes sqlite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_01_085654) do
-  create_table "floors", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema[7.0].define(version: 0) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
 end
