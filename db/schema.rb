f513d87f6e04e2e1b115ec7044a0fc2b1ce5045f
Xi Chu <xi.chu@tamu.edu>
2022-04-30 23:58:28 +0000

    remove uncessary files and article model/view/controller, and regenerate database

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_20_083045) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "plan_models", force: :cascade do |t|
     t.string "name"
